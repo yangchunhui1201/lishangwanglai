7d4c92fa7cd2021e6fb8694ff86fa197cb537fef
yangchunhui1201 <1752619389@qq.com>
2020-02-27 19:05:02 +0800

    理事理财

--- 礼尚往来/app/src/component/Manage/Manage .jsx
+++ 礼尚往来/app/src/component/Manage/Manage .jsx
@@ -1,11 +1,89 @@
 import React,{Component} from 'react'
+import './Licai.css'
 export default class Manage extends Component{
 constructor(){
     super()
 }
 render(){
     return(
-        <div>理账</div>
+        <div>
+			<div class="header">
+				<p class="left">fuhao</p>
+				
+					<span class="ban">收礼</span>
+					<span class="ban1">送礼</span>
+				
+				<p class="right">fuhao</p>
+			</div>
+			<div class="list">
+				<div class="list-top">
+					<div class="top-con">
+						<p class="top-le">类目名称</p>
+						<p class="bot-ri">15000.00</p>
+					</div>
+					<div class="bootton-con">
+						<p class="top-le">姓名</p>
+						<p class="bot-rig">
+							<span>10人送礼    </span>
+							<span>20165.12</span>
+						</p>
+					</div>
+				</div>
+				<div class="list-top">
+					<div class="top-con">
+						<p class="top-le">类目名称</p>
+						<p class="bot-ri">15000.00</p>
+					</div>
+					<div class="bootton-con">
+						<p class="top-le">姓名</p>
+						<p class="bot-rig">
+							<span>10人送礼    </span>
+							<span>20165.12</span>
+						</p>
+					</div>
+				</div>
+				<div class="list-top">
+					<div class="top-con">
+						<p class="top-le">类目名称</p>
+						<p class="bot-ri">15000.00</p>
+					</div>
+					<div class="bootton-con">
+						<p class="top-le">姓名</p>
+						<p class="bot-rig">
+							<span>10人送礼    </span>
+							<span>20165.12</span>
+						</p>
+					</div>
+				</div>
+				<div class="list-top">
+					<div class="top-con">
+						<p class="top-le">类目名称</p>
+						<p class="bot-ri">15000.00</p>
+					</div>
+					<div class="bootton-con">
+						<p class="top-le">姓名</p>
+						<p class="bot-rig">
+							<span>10人送礼    </span>
+							<span>20165.12</span>
+						</p>
+					</div>
+				</div>
+				<div class="list-top">
+					<div class="top-con">
+						<p class="top-le">类目名称</p>
+						<p class="bot-ri">15000.00</p>
+					</div>
+					<div class="bootton-con">
+						<p class="top-le">姓名</p>
+						<p class="bot-rig">
+							<span>10人送礼    </span>
+							<span>20165.12</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		
+		</div>
     )
 }
 }
--- 礼尚往来/app/src/component/director/director.jsx
+++ 礼尚往来/app/src/component/director/director.jsx
@@ -1,11 +1,49 @@
 import React,{Component} from 'react'
+import './Lishi.css'
 export default class director extends Component{
 constructor(){
     super()
 }
 render(){
     return(
-        <div>礼事</div>
+        <div>
+			<div class="header">
+				<p class="left">fuhao</p>
+				
+					<span class="ban">待参加</span>
+					<span class="ban1">已参加</span>
+				
+				<p class="right">fuhao</p>
+			</div>
+			<div class="list">
+				<div class="list-top">
+					<div class="top-con">
+						<p class="top-le">王大锤搬家请客</p>
+						<p class="bot-ri1">待参加</p>
+					</div>
+					<div class="bootton-con">
+						<p class="top-le">地点名称</p>
+						<p class="bot-rig">
+							<span>2016.8.21</span>
+						</p>
+					</div>
+				</div>
+				<div class="list-top">
+					<div class="top-con">
+						<p class="top-le">王大锤搬家请客</p>
+						<p class="bot-ri1">已参加</p>
+					</div>
+					<div class="bootton-con">
+						<p class="top-le">地点名称</p>
+						<p class="bot-rig">
+							<span>2016.6.21</span>
+						</p>
+					</div>
+				</div>
+			
+			</div>
+					
+		</div>
     )
 }
 }
