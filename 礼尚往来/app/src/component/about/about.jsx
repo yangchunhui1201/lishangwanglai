6407dd192e243390d0f5c408d674a513a8b8e288
yangchunhui1201 <1752619389@qq.com>
2020-03-01 20:51:54 +0800

    布局

--- 礼尚往来/app/src/component/about/about.jsx
+++ 礼尚往来/app/src/component/about/about.jsx
@@ -1,11 +1,45 @@
 import React,{Component} from 'react'
+import './about.css'
+import {NavLink} from 'react-router-dom'
 export default class about extends Component{
 constructor(){
     super()
 }
 render(){
     return(
-        <div>我的</div>
+		<div>
+			<div id='me'>
+				<div id="me-head">我的</div>
+			</div>
+			<div id="dengl">
+				<div id="dengl-span">调用QQ,微信昵称</div>
+			</div>
+			<NavLink to="/sz">
+				<div id="gengd">
+					<div id="gengd-span">设置</div>
+				</div>
+			</NavLink>
+			<NavLink to="/fk">
+				<div id="gengd">
+					<div id="gengd-span">反馈</div>
+				</div>
+			</NavLink>
+			<NavLink to='/bj'>
+				<div id="gengd">
+					<div id="gengd-span">帮助</div>
+				</div>
+			</NavLink>
+			<NavLink to='/gy'>
+				<div id="gengd">
+					<div id="gengd-span">关于</div>
+				</div>
+			</NavLink>
+			<NavLink to="/fx">
+				<div id="gengd">
+					<div id="gengd-span">分享</div>
+				</div>
+			</NavLink>
+		</div>
     )
 }
 }
